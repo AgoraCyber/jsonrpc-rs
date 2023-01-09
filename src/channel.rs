a0d4b17e24d01013d621f40e88202bbab586c70f
yayanyang <yayanyang@gmail.com>
2023-01-09 20:35:05 +0800

    add transport channel trait

--- src/channel.rs
+++ src/channel.rs
@@ -1,2 +1,17 @@
+use futures::{Sink, Stream};
+use std::future::Future;
+
 /// Define abstract transport channel for [`crate::Client`] and [`crate::Server`]
-pub trait TransportChannel {}
+pub trait TransportChannel {
+    /// Transport channel error type.
+    type Error: std::error::Error + 'static + Sync + Send;
+
+    /// Input stream must support [`Send`] + [`Sync`]
+    type Input: Stream<Item = Result<String, Self::Error>> + Unpin + Send + Sync + 'static;
+
+    type Output: Sink<String, Error = Self::Error> + Unpin + Send + Sync + 'static;
+
+    fn spawn<Fut>(&self, future: Fut)
+    where
+        Fut: Future<Output = ()> + Send + 'static;
+}
